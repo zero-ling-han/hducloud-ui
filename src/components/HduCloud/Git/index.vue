c6ac65c5f0b5b14e9510afa767d1a366c26bd644
hanjiyu <2017958049@qq.com>
2023-03-02 17:37:22 +0800

    init hducloud-ui

---
+++ src/components/HduCloud/Git/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="github" @click="goto" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HduCloudGit',
+  data() {
+    return {
+      url: 'https://gitee.com/tony2y/HduCloud-flowable'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
